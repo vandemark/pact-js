85619007884bc1036f687186e6802b344aa03b55
Matt Fellows <matt.fellows@onegeek.com.au>
2019-03-17 11:16:53 +1100

    update example with 'getMsg' function to match details in #271

--- examples/messages/provider/message-provider.spec.ts
+++ examples/messages/provider/message-provider.spec.ts
@@ -1,15 +1,15 @@
 /* tslint:disable:no-unused-expression object-literal-sort-keys max-classes-per-file no-empty no-console*/
 const { MessageProviderPact } = require("../../../src/pact")
 import path = require("path")
-const { createDog } = require("./dog-client")
+const { getMsg } = require("./msg-client")
 
 describe("Message provider tests", () => {
   const p = new MessageProviderPact({
     messageProviders: {
-      "a request for rendering": () => createDog(27),
+      "a request for rendering": () => getMsg(),
     },
     log: path.resolve(process.cwd(), "logs"),
-    logLevel: "INFO",
+    logLevel: "fatal",
     provider: "MyJSMessageProvider",
     providerVersion: "1.0.0",
 
@@ -22,9 +22,7 @@ describe("Message provider tests", () => {
     ],
   })
 
-  describe("renders", () => {
-    it("sends a rendering event", () => {
-      return p.verify()
-    })
+  describe("send a render message", () => {
+    it("sends a valid render message", () => p.verify())
   })
 })
